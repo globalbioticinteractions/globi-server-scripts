ec266de97a213a52552db4b01203906ccda818f2
Jorrit Poelen <jhpoelen@gmail.com>
2015-12-17 12:43:04 -0800

    build empty ramdisk script

---
+++ build-ramdisk.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+RAM_DISK=/var/cache/globi/ramdisk
+/bin/umount tmpfs0
+/bin/rm -rf $RAM_DISK
+/bin/mkdir -p $RAM_DISK
+/bin/mount -t tmpfs -o size=8G tmpfs0 $RAM_DISK
+
+/bin/chown -R jhpoelen:jhpoelen $RAM_DISK
+/bin/chmod -R g+rwx $RAM_DISK
