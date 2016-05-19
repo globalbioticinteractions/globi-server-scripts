690dfaad381a9e214215649ce487895b2c480523
Jorrit Poelen <jhpoelen@gmail.com>
2016-05-19 12:29:41 -0700

    add support for zip archives

--- refresh_data.sh
+++ refresh_data.sh
@@ -3,11 +3,12 @@ CACHE_DIR=/var/cache
 NEO4J_CACHE_DIR=$CACHE_DIR/neo4j
 MAVEN_REPO=/home/jhpoelen/.m2/repository
 
-GRAPH_DB_TAR=$NEO4J_CACHE_DIR/graph.db.tar.gz
-GRAPH_DB_TAR_NEW=$NEO4J_CACHE_DIR/graph.db.new.tar.gz
+GRAPH_DB_EXT=zip
+GRAPH_DB_TAR=$NEO4J_CACHE_DIR/graph.db.$GRAPH_DB_EXT
+GRAPH_DB_TAR_NEW=$NEO4J_CACHE_DIR/graph.db.new.$GRAPH_DB_EXT
 
 # grab data
-cp $MAVEN_REPO/org/eol/eol-globi-datasets/1.0-SNAPSHOT/eol-globi-datasets-1.0-SNAPSHOT-neo4j-graph-db.tar.gz $GRAPH_DB_TAR_NEW
+cp $MAVEN_REPO/org/eol/eol-globi-datasets/1.0-SNAPSHOT/eol-globi-datasets-1.0-SNAPSHOT-neo4j-graph-db.$GRAPH_DB_EXT $GRAPH_DB_TAR_NEW
 chown neo4j:nogroup $GRAPH_DB_TAR_NEW 
 
 if diff $GRAPH_DB_TAR $GRAPH_DB_TAR_NEW >/dev/null ; then
@@ -17,7 +18,11 @@ else
   sudo -u neo4j cp $GRAPH_DB_TAR_NEW $GRAPH_DB_TAR
   sudo -u neo4j /usr/sbin/service neo4j-service stop
   sudo -u neo4j rm -rf $NEO4J_CACHE_DIR/graph.db
-  sudo -u neo4j tar -xvf $GRAPH_DB_TAR -C $NEO4J_CACHE_DIR
+  if [ "$GRAPH_DB_EXT" -eq "zip" ] then
+    sudo -u neo4j zip $GRAPH_DB_TAR -d $NEO4J_CACHE_DIR
+  else 
+    sudo -u neo4j tar -xvf $GRAPH_DB_TAR -C $NEO4J_CACHE_DIR
+  fi
   sudo -u neo4j /usr/sbin/service neo4j-service start
   # reset nginx cache
   sudo rm -rf /var/cache/nginx
