261f2944712c96caf8ccc35aa7d32169f44545bd
Jorrit Poelen <jhpoelen@gmail.com>
2017-09-18 19:50:35 -0700

    Update build-ramdisk.sh

--- build-ramdisk.sh
+++ build-ramdisk.sh
@@ -3,7 +3,7 @@ RAM_DISK=/var/cache/globi/ramdisk
 /bin/umount tmpfs0
 /bin/rm -rf $RAM_DISK
 /bin/mkdir -p $RAM_DISK
-/bin/mount -t tmpfs -o size=8G tmpfs0 $RAM_DISK
+/bin/mount -t tmpfs -o size=10G tmpfs0 $RAM_DISK
 
 /bin/chown -R jhpoelen:jhpoelen $RAM_DISK
 /bin/chmod -R g+rwx $RAM_DISK
