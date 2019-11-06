1fe2ac542267eadfe40a9e2284143366ccc87026
jhpoelen <jhpoelen@xs4all.nl>
2019-11-06 10:27:28 -0800

    attempt to make updates faster

--- refresh_data.sh
+++ refresh_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CACHE_DIR=/var/cache
 NEO4J_CACHE_DIR=$CACHE_DIR/neo4j
+NEO4J_CACHE_UPDATE_DIR=$CACHE_DIR/neo4j.update
 MAVEN_REPO=/home/jhpoelen/.m2/repository
 
 GRAPH_DB_EXT=zip
@@ -16,10 +17,12 @@ if diff $GRAPH_DB_ARCHIVE $GRAPH_DB_ARCHIVE_NEW >/dev/null ; then
 else
   echo File different updating
   sudo -u neo4j cp $GRAPH_DB_ARCHIVE_NEW $GRAPH_DB_ARCHIVE
+  sudo -u neo4j rm -rf $NEO4J_CACHE_UPDATE_DIR && sudo -u neo4j mkdir -p $NEO4J_CACHE_UPDATE_DIR
+  sudo -u neo4j unzip $GRAPH_DB_ARCHIVE -d $NEO4J_CACHE_UPDATE_DIR
   sudo /usr/sbin/service neo4j stop
   echo $(date) installing new neo4j data index...
-  sudo -u neo4j rm -rf $NEO4J_CACHE_DIR/graph.db
-  sudo -u neo4j unzip $GRAPH_DB_ARCHIVE -d $NEO4J_CACHE_DIR
+  sudo -u neo4j mv $NEO4J_CACHE_DIR/graph.db $NEO4J_CACHE_UPDATE_DIR/graph.db.old
+  sudo -u neo4j mv $NEO4J_CACHE_UPDATE_DIR/graph.db $NEO4J_CACHE_DIR/graph.db
   echo $(date) installing new neo4j data index done.
   sudo /usr/sbin/service neo4j start
   echo $(date) resetting nginx cache...
