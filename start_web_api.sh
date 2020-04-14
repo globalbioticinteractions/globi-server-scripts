9d8cd363c1a69b3389b456c81773a4a203e39a41
jhpoelen <jhpoelen@xs4all.nl>
2020-04-14 11:11:55 -0700

    no need to install artifacts

--- start_web_api.sh
+++ start_web_api.sh
@@ -4,7 +4,7 @@ JETTY_DIR="$JETTY_HOME/target"
 
 cd "$GLOBI_HOME"
 echo jetty rebuilding...
-mvn clean install -pl eol-globi-rest -am -DskipTests 
+mvn clean package -pl eol-globi-rest -am -DskipTests 
 
 echo jetty starting...
 java -Dneo4j.cypher.uri="http://localhost:7476/db/data/cypher" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 $JETTY_DIR/*.war 
