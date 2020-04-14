3fb8ca426f345da1d3d6c28f7c08bd42b0d9e006
jhpoelen <jhpoelen@xs4all.nl>
2020-04-14 11:30:38 -0700

    bind to localhost

--- start_web_api.sh
+++ start_web_api.sh
@@ -7,6 +7,6 @@ echo jetty rebuilding...
 mvn clean package -pl eol-globi-rest -am -DskipTests 
 
 echo jetty starting...
-java -Dneo4j.cypher.uri="http://localhost:7476/db/data/cypher" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 $JETTY_DIR/*.war 
+java -Dneo4j.cypher.uri="http://localhost:7476/db/data/cypher" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 --host localhost $JETTY_DIR/*.war 
 
 
