b8be241adb780e09b8fca71ab21efe648bb67d3d
jhpoelen <jhpoelen@xs4all.nl>
2020-04-16 12:56:22 -0700

    remove nohup from sparql startup

--- start-sparql-endpoint.sh
+++ start-sparql-endpoint.sh
@@ -6,16 +6,11 @@ fi
 
 FUSEKI_HOME="$GLOBI_HOME/eol-globi-rdf"
 FUSEKI_DIR="$FUSEKI_HOME/target/apache-jena-fuseki-2.4.0"
-FUSEKI_PID="$FUSEKI_DIR/fuseki.pid"
-PID=$(cat "$FUSEKI_PID" 2>/dev/null)
-kill "$PID" 2>/dev/null
 
 cd "$GLOBI_HOME"
 echo fuseki rebuilding...
-mvn clean install -pl eol-globi-rdf -Prdf
+mvn clean install -pl eol-globi-rdf -am -Prdf
 
 echo fuseki starting...
 cd $FUSEKI_DIR
-nohup java -Xmx12G -jar fuseki-server.jar --localhost --config="config.ttl" &> $FUSEKI_DIR/eol-globi-rdf.log &
-echo $! > "$FUSEKI_PID"
-echo fuseki started.
+java -Xmx12G -jar fuseki-server.jar --localhost --config="config.ttl" &> $FUSEKI_DIR/eol-globi-rdf.log &
