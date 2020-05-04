bdc054aa62dc876abe7e4066b57dcf114952cc91
jhpoelen <jhpoelen@xs4all.nl>
2020-05-04 10:49:05 -0700

    increase ramdisk space

--- build-ramdisk.sh
+++ build-ramdisk.sh
@@ -3,7 +3,7 @@ RAM_DISK=/var/cache/globi/ramdisk
 /bin/umount tmpfs0
 /bin/rm -rf $RAM_DISK
 /bin/mkdir -p $RAM_DISK
-/bin/mount -t tmpfs -o size=18G tmpfs0 $RAM_DISK
+/bin/mount -t tmpfs -o size=20G tmpfs0 $RAM_DISK
 
 /bin/chown -R jhpoelen:jhpoelen /var/cache/globi
 /bin/chmod -R g+rwx $RAM_DISK
