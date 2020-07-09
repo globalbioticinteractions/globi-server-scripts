0419a773e9671802b8600031a8248792df0ef7b1
jhpoelen <jhpoelen@xs4all.nl>
2020-07-09 10:14:20 -0700

    add s3 cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,7 @@
-rm /home/jhpoelen/export-eol-globi-data.log
-rm /home/jhpoelen/index-taxa.log
 # removes files older than 30 days from globi repo
-find ~/.m2/repository/org/eol/* -mtime +30 | xargs rm
+# find ~/.m2/repository/org/eol/* -mtime +30 | xargs rm
+
+# create a list of snaphots using some regex
+DATE_PATTERN='\-20200[12345]'
+aws s3 ls globi/snapshot/org/eol/eol-globi-datasets/1.0-SNAPSHOT/ | sed s+^.*eol-+eol-+g | grep "$DATE_PATTERN" | sed 's+^+s3://globi/snapshot/org/eol/eol-globi-datasets/1.0-SNAPSHOT/+g' > aws-delete-candidates.tsv
+
