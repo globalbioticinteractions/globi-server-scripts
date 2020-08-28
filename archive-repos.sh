aa82589015d9c0e9a6d9ea9026db787137f46682
jhpoelen <jhpoelen@xs4all.nl>
2020-08-27 17:54:49 -0700

    add limit-aware archive programw

--- archive-repos.sh
+++ archive-repos.sh
@@ -3,6 +3,17 @@
 # Requests GloBI dataset registration repositories to be registered with the https://softwareheritage.org .
 #
 
-REPOS=$(elton ls --cache-dir=/var/cache/elton/datasets) 
 
-echo -e "$REPOS" | xargs -L1 -I{} curl --verbose -XPOST -L --verbose "https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/{}"
+LIMITS=$(curl -i https://archive.softwareheritage.org/api/1/stat/counters/ | grep ^X-RateLimit)
+REMAINING=$(echo -n "$LIMITS" | grep "Remaining:" | grep -Eo "[0-9]*" | tr -d '\n')
+
+
+if [ "$REMAINING" -gt 0 ] ; then
+  REPOS=$(elton ls --cache-dir=/var/cache/elton/datasets | shuf -n $REMAINING)
+  echo "requesting softwareheritage.org to archive [$REMAINING] GloBI repos: $REPOS"
+  echo -e "$REPOS" | xargs -L1 -I{} curl --verbose -XPOST -L --verbose "https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/{}"
+else
+  echo no requests left, skipping archive requests
+fi
+
+
