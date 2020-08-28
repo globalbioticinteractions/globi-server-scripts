452c4427402045f3305e1eac9721f182efae00a9
jhpoelen <jhpoelen@xs4all.nl>
2020-08-27 17:56:58 -0700

    debug

--- archive-repos.sh
+++ archive-repos.sh
@@ -11,7 +11,7 @@ REMAINING=$(echo -n "$LIMITS" | grep "Remaining:" | grep -Eo "[0-9]*" | tr -d '\
 if [ "$REMAINING" -gt 0 ] ; then
   REPOS=$(elton ls --cache-dir=/var/cache/elton/datasets | shuf -n $REMAINING)
   echo "requesting softwareheritage.org to archive [$REMAINING] GloBI repos: $REPOS"
-  echo -e "$REPOS" | xargs -L1 -I{} curl --verbose -XPOST -L --verbose "https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/{}"
+  #echo -e "$REPOS" | xargs -L1 -I{} curl --verbose -XPOST -L --verbose "https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/{}"
 else
   echo no requests left, skipping archive requests
 fi
