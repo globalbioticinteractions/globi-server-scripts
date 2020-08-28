f47caf65a812ce64841132b3dbbcba9ba289bc37
jhpoelen <jhpoelen@xs4all.nl>
2020-08-27 17:15:15 -0700

    add archive script

---
+++ archive-repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Requests GloBI dataset registration repositories to be registered with the https://softwareheritage.org .
+#
+
+REPOS=$(elton ls --cache-dir=/var/cache/elton/datasets) 
+
+echo $REPOS | xargs -L1 -I{} echo curl -XPOST -L --verbose "https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/{}"
