64e393a7dd2ca6ba54d8e62b483c79e815653082
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 11:46:52 -0700

    fix syntax

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -2,7 +2,7 @@
 #
 # Updates only datasets that are not yet known to local elton cache.
 #
-set -xe
+set -x
 
 DATASETS_ONLINE=$(elton ls --online --no-progress | sort | uniq | grep -v "^local$")
 DATASETS_LOCAL=$(elton ls --no-progress | sort | uniq | grep -v "^local$")
@@ -13,7 +13,7 @@ DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(ec
 if [ $(echo -e ${DATASETS_NEW} | wc -l) -gt 0 ]
 then
   echo "found new datasets"
-  echo -e "${DATASETS_NEW}" | xargs elton update --no-progress"
+  echo -e "${DATASETS_NEW}" | xargs elton update --no-progress
 else
   echo "no new datasets found: not updating"
 fi
