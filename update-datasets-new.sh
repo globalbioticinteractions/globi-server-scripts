8d656899f49fe1a6891c07c7f4f366720833b49c
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 10:31:13 -0700

    pass at only updating new datasets

---
+++ update-datasets-new.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Updates only datasets that are not yet known to local elton cache.
+#
+CACHE_DIR=${ELTON_DATASET_DIR:/var/cache/elton/datasets}
+
+DATASETS_ONLINE=$(elton ls --online)
+DATASETS_LOCAL=$(elton ls --data-dir ${CACHE_DIR})
+
+DATASETS_NEW=$(diff --changed-group-format='%>' <(${DATASETS_LOCAL}) <(${DATASETS_ONLINE}))
+
+echo ${DATASETS_NEW} | xargs -L1 echo elton update 
