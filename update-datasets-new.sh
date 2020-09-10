eac099e2f37d4ee17bcdad06cbecbd368b03cf15
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 10:36:41 -0700

    fix cache dir

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -2,10 +2,11 @@
 #
 # Updates only datasets that are not yet known to local elton cache.
 #
+set -xe
 CACHE_DIR=${ELTON_DATASET_DIR:=/var/cache/elton/datasets}
 
 DATASETS_ONLINE=$(elton ls --online)
-DATASETS_LOCAL=$(elton ls --data-dir ${CACHE_DIR})
+DATASETS_LOCAL=$(elton ls --cache-dir ${CACHE_DIR})
 
 DATASETS_NEW=$(diff --changed-group-format='%>' <(${DATASETS_LOCAL}) <(${DATASETS_ONLINE}))
 
