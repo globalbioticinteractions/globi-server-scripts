cce9850e6ae752f1036f376736536b200fee6752
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 11:37:36 -0700

    update update script and systemd

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -3,18 +3,17 @@
 # Updates only datasets that are not yet known to local elton cache.
 #
 set -xe
-CACHE_DIR=${ELTON_DATASET_DIR:=/var/cache/elton/datasets}
 
-DATASETS_ONLINE=$(elton ls --online | sort | uniq | grep -v "^local$")
-DATASETS_LOCAL=$(elton ls --cache-dir ${CACHE_DIR} | sort | uniq | grep -v "^local$")
+DATASETS_ONLINE=$(elton ls --online --no-progress | sort | uniq | grep -v "^local$")
+DATASETS_LOCAL=$(elton ls --no-progress | sort | uniq | grep -v "^local$")
 
 # note the "!" operator stops the script when no new datasets are found
-DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}") && true)
+DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}") | true)
 
 if [ $(echo -e ${DATASETS_NEW} | wc -l) -gt 0 ]
 then
   echo "found new datasets"
-  echo -e "${DATASETS_NEW}" | xargs elton update
+  echo -e "${DATASETS_NEW}" | xargs elton update --no-progress"
 else
   echo "no new datasets found: not updating"
 fi
