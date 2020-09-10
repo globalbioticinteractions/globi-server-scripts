023dae8eef164634cd6dc644ea374cf021ce4dc3
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 11:58:03 -0700

    use character count instead of linecount to detect new datasets

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -10,7 +10,7 @@ DATASETS_LOCAL=$(elton ls --no-progress | sort | uniq | grep -v "^local$")
 # note the "!" operator stops the script when no new datasets are found
 DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}"))
 
-if [ $(echo -e ${DATASETS_NEW} | wc -l) -gt 0 ]
+if [ $(echo -e ${DATASETS_NEW} | wc -m) -gt 0 ]
 then
   echo "found new datasets"
   echo -e "${DATASETS_NEW}" | xargs elton update --no-progress
