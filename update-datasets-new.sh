51e0f821d9791dff793f8078ced7e98750989371
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 11:29:15 -0700

    add logic for update

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -8,7 +8,13 @@ CACHE_DIR=${ELTON_DATASET_DIR:=/var/cache/elton/datasets}
 DATASETS_ONLINE=$(elton ls --online | sort | uniq | grep -v "^local$")
 DATASETS_LOCAL=$(elton ls --cache-dir ${CACHE_DIR} | sort | uniq | grep -v "^local$")
 
-DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}") | true)
+# note the "!" operator stops the script when no new datasets are found
+DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}") && true)
 
-echo -e "${DATASETS_NEW}" | xargs elton update
-
+if [ $(echo -e ${DATASETS_NEW} | wc -l) -gt 0 ]
+then
+  echo "found new datasets"
+  echo -e "${DATASETS_NEW}" | xargs elton update
+else
+  echo "no new datasets found: not updating"
+fi
