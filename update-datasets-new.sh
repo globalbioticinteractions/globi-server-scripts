71fc9e2f272b97d94ba14638c27a2ed5fdc11798
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 11:08:21 -0700

    ignore return code for diff to prevent premature exit

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -8,7 +8,7 @@ CACHE_DIR=${ELTON_DATASET_DIR:=/var/cache/elton/datasets}
 DATASETS_ONLINE=$(elton ls --online | sort | uniq | grep -v "^local$")
 DATASETS_LOCAL=$(elton ls --cache-dir ${CACHE_DIR} | sort | uniq | grep -v "^local$")
 
-DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}"))
+DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}") | true)
 
 echo -e "${DATASETS_NEW}" | xargs elton update
 
