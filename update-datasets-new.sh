681b0b6e9d2aab0f070cc0d2b36ad34986220818
jhpoelen <jhpoelen@xs4all.nl>
2020-09-10 10:51:57 -0700

    exclude local

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -5,9 +5,9 @@
 set -xe
 CACHE_DIR=${ELTON_DATASET_DIR:=/var/cache/elton/datasets}
 
-DATASETS_ONLINE=$(elton ls --online | sort | uniq | tee online.tsv)
-DATASETS_LOCAL=$(elton ls --cache-dir ${CACHE_DIR} | sort | uniq | tee local.tsv)
+DATASETS_ONLINE=$(elton ls --online | sort | uniq | grep -v "^local$" | tee online.tsv)
+DATASETS_LOCAL=$(elton ls --cache-dir ${CACHE_DIR} | sort | uniq | grep -v "^local$" | tee local.tsv)
 
-DATASETS_NEW=$(diff --changed-group-format='%>' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}"))
+DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo -e "${DATASETS_LOCAL}") <(echo -e "${DATASETS_ONLINE}"))
 
 echo -e "${DATASETS_NEW}" | xargs -L1 echo elton update 
