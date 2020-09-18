44ac765ea9da6f9f9f7a626fedf6f485b9a72108
jhpoelen <jhpoelen@xs4all.nl>
2020-09-18 16:24:58 -0700

    increase quiet time

--- elton-update-ready.sh
+++ elton-update-ready.sh
@@ -7,14 +7,12 @@ set -x
 
 function is_quiet() {
   local LAST_MODIFIED_AT=$(find . -type f -name access.tsv -printf "%T@\n" | sort -nr | sed 's/\..*//g' | head -1)
-  local QUIET_TIME_IN_SECONDS=$(( 20*60 ))
-  local QUIET_TIME_IN_SECONDS=$(( 20*60 ))
-  local QUIET_TIME_IN_SECONDS=$(( 10 ))
+  local QUIET_TIME_IN_SECONDS=$(( 5*60 ))
   local SECONDS_SINCE_LAST_CHANGE=$(( $(date +%s) - ${LAST_MODIFIED_AT:=0} ))
 
   if [ $QUIET_TIME_IN_SECONDS -gt $SECONDS_SINCE_LAST_CHANGE ] 
   then
-    echo elton is updating: last change [${SECONDS_SINCE_LAST_CHANGE}]s ago
+    echo elton update in progress: last change [${SECONDS_SINCE_LAST_CHANGE}]s ago
     exit 1
   fi
 }
@@ -28,11 +26,12 @@ function content_same() {
   SAME_CONTENT=$?
   if [ $SAME_CONTENT == 0 ]
     then
-      echo no change
+      echo no change: no update needed
       exit 1
   fi
 }
 
 is_quiet
 content_same
+echo elton has updates
 exit 0
