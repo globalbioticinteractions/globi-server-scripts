56cb1725d3dd0279e5a64c14f03c85f47f9ec947
jhpoelen <jhpoelen@xs4all.nl>
2020-12-29 12:24:44 -0800

    fix reference

--- review-datasets-all.sh
+++ review-datasets-all.sh
@@ -6,5 +6,5 @@ set -x
 DATASET_DIR=${ELTON_DATASET_DIR:-/var/cache/elton/datasets}
 DATASETS_LOCAL=$(elton ls --cache-dir "${DATASET_DIR}" --no-progress | sort | uniq | grep -v "^local$")
 
-echo "$DATASETS_LOCAL" | head -n2 | xargs -L1 -I '{}' /bin/bash /var/lib/globinizer/check-datasets.sh '{}' ${DATASET_DIR}
+echo "$DATASETS_LOCAL" | head -n2 | xargs -L1 -I '{}' /bin/bash /var/lib/globinizer/check-dataset.sh '{}' ${DATASET_DIR}
 
