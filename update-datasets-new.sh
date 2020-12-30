12c4b6d99524674e91b7b9016cc6fa0602c551b7
jhpoelen <jhpoelen@xs4all.nl>
2020-12-29 16:54:20 -0800

    add reviews to incremental updates

--- update-datasets-new.sh
+++ update-datasets-new.sh
@@ -13,7 +13,14 @@ DATASETS_NEW=$(diff --changed-group-format='%>' --unchanged-group-format='' <(ec
 if [ $(echo -en ${DATASETS_NEW} | wc -m) -gt 0 ]
 then
   echo "found new datasets"
+  echo "updating..."
   echo -e "${DATASETS_NEW}" | xargs elton update --no-progress
+  echo "updating... done."
+  
+  echo "indexing..."
+  DATASET_DIR=${ELTON_DATASET_DIR:-/var/cache/elton/datasets}
+  echo "$DATASETS_NEW" | xargs -L1 -I '{}' /bin/bash /var/lib/globinizer/check-dataset.sh '{}' ${DATASET_DIR}
+  echo "indexing... done."
   exit 0
 else
   echo "no new datasets found: not updating"
