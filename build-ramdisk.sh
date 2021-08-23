008f919bec1a5b1b4342ff124084918ccdbdede5
jhpoelen <jhpoelen@xs4all.nl>
2021-08-23 10:21:53 -0700

    bump ramdisk to 32G

--- build-ramdisk.sh
+++ build-ramdisk.sh
@@ -3,7 +3,7 @@ RAM_DISK=/var/cache/globi/ramdisk
 /bin/umount tmpfs0
 /bin/rm -rf $RAM_DISK
 /bin/mkdir -p $RAM_DISK
-/bin/mount -t tmpfs -o size=26G tmpfs0 $RAM_DISK
+/bin/mount -t tmpfs -o size=32G tmpfs0 $RAM_DISK
 
 /bin/chown -R globi:globi $RAM_DISK
 /bin/chmod -R g+rwx $RAM_DISK
