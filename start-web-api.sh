c181381678b3138503a00e204f20f825ef412abd
jhpoelen <jhpoelen@xs4all.nl>
2021-09-14 02:48:57 -0700

    fix web api to specific commit hash

--- start-web-api.sh
+++ start-web-api.sh
@@ -2,9 +2,17 @@
 JETTY_HOME="$GLOBI_HOME/eol-globi-rest"
 JETTY_DIR="$JETTY_HOME/target"
 
+# set specific version of GloBI indexer to use
+COMMIT_HASH=3c8cb8ec8fb5facb15b0f16f30afe524a9047583
+
 cd "$GLOBI_HOME"
 echo updating git ...
 git pull --rebase
+
+# checkout specific version if provided
+if [ -n "$COMMIT_HASH" ]; then
+  git checkout "$COMMIT_HASH"
+fi
 
 echo jetty rebuilding...
 mvn clean package -pl eol-globi-rest -am --settings /etc/globi/.m2/settings.xml -DskipTests  
