68f739a465ad03c5124795956e055a50acfce466
jhpoelen <jhpoelen@xs4all.nl>
2021-09-16 15:50:14 -0700

    switch to transactional cypher endpoint; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/701

--- start-web-api.sh
+++ start-web-api.sh
@@ -2,6 +2,8 @@
 JETTY_HOME="$GLOBI_HOME/eol-globi-rest"
 JETTY_DIR="$JETTY_HOME/target"
 
+NEO4J_PORT=7474
+
 # set specific version of GloBI indexer to use
 #COMMIT_HASH=3c8cb8ec8fb5facb15b0f16f30afe524a9047583
 
@@ -18,6 +20,6 @@ echo jetty rebuilding...
 mvn clean package -pl eol-globi-rest -am --settings /etc/globi/.m2/settings.xml -DskipTests  
 
 echo jetty starting...
-java -Dneo4j.cypher.uri="http://localhost:7476/db/data/cypher" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 --host localhost $JETTY_DIR/*.war 
+java -Dneo4j.cypher.uri="http://localhost:${NEO4J_PORT}/db/data/transaction/commit" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 --host localhost $JETTY_DIR/*.war 
 
 
