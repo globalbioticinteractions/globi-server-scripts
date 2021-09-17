11496118a3bbac18402be8a0ac5db2539425f74f
jhpoelen <jhpoelen@xs4all.nl>
2021-09-16 22:15:36 -0700

    restart on unhappy neo4j

--- check-neo4j.sh
+++ check-neo4j.sh
@@ -5,3 +5,13 @@
 echo "CYPHER 2.3 start d = node:datasets('namespace:\"globalbioticinteractions/template-dataset\"') return d.namespace limit 1;"\
  | cypher-shell\
  | grep "template-dataset"
+
+NEO4J_HAPPY=$?
+echo $NEO4J_HAPPY
+
+ if [[ ${NEO4J_HAPPY} -eq 0 ]]; then
+    echo neo4j is happy
+ else
+    echo neo4j is not happy
+    systemctl restart neo4j
+ fi
