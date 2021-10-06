437f07cde8f23a10360ed865f189c3d2257ae93f
jhpoelen <jhpoelen@xs4all.nl>
2021-10-05 23:30:50 -0700

    switch back to 7477

--- start-web-api.sh
+++ start-web-api.sh
@@ -2,7 +2,8 @@
 JETTY_HOME="$GLOBI_HOME/eol-globi-rest"
 JETTY_DIR="$JETTY_HOME/target"
 
-NEO4J_PORT=7478
+NEO4J_PORT=7477
+NEO4J_HOST=localhost
 
 # set specific version of GloBI indexer to use
 #COMMIT_HASH=3c8cb8ec8fb5facb15b0f16f30afe524a9047583
@@ -20,6 +21,6 @@ echo jetty rebuilding...
 mvn clean package -pl eol-globi-rest -am --settings /etc/globi/.m2/settings.xml -DskipTests  
 
 echo jetty starting...
-java -Dneo4j.cypher.uri="http://localhost:${NEO4J_PORT}/db/data/transaction/commit" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 --host localhost $JETTY_DIR/*.war 
+java -Dneo4j.cypher.uri="http://${NEO4J_HOST}:${NEO4J_PORT}/db/data/transaction/commit" -jar $JETTY_DIR/dependency/jetty-runner.jar --port 8080 --host localhost $JETTY_DIR/*.war 
 
 
