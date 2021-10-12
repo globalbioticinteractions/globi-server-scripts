81febba20ef035e16246ffbfc02c0ebb3d9b4db1
jhpoelen <jhpoelen@xs4all.nl>
2021-10-12 15:48:18 -0700

    set fuseki version to 3.17.0

--- start-sparql-endpoint.sh
+++ start-sparql-endpoint.sh
@@ -5,7 +5,8 @@ if [ -z "$GLOBI_HOME" ]; then
 fi
 
 FUSEKI_HOME="$GLOBI_HOME/eol-globi-rdf"
-FUSEKI_DIR="$FUSEKI_HOME/target/apache-jena-fuseki-4.2.0"
+FUSEKI_VERSION="3.17.0"
+FUSEKI_DIR="$FUSEKI_HOME/target/apache-jena-fuseki-${FUSEKI_VERSION}"
 
 cd "$GLOBI_HOME"
 echo fuseki rebuilding...
