55f5cbfec6ad750341ba9481396a56eb2ef01c92
jhpoelen <jhpoelen@xs4all.nl>
2021-10-12 15:56:10 -0700

    truncate interactions.nq.gz for testing

--- start-sparql-endpoint.sh
+++ start-sparql-endpoint.sh
@@ -7,10 +7,15 @@ fi
 FUSEKI_HOME="$GLOBI_HOME/eol-globi-rdf"
 FUSEKI_VERSION="3.17.0"
 FUSEKI_DIR="$FUSEKI_HOME/target/apache-jena-fuseki-${FUSEKI_VERSION}"
+FUSEKI_DATA="$FUSEKI_HOME/target/apache-jena-fuseki-${FUSEKI_VERSION}/Data/interactions.nq.gz"
 
 cd "$GLOBI_HOME"
 echo fuseki rebuilding...
 mvn clean install -pl eol-globi-rdf -am -Prdf --settings /etc/globi/.m2/settings.xml
+
+# truncate interactions.nq.gz to 1M lines for now.
+cat ${FUSEKI_DATA} | gunzip | head -n1000000 | gzip > ${FUSEKI_DATA}.new
+mv ${FUSEKI_DATA}.new ${FUSEKI_DATA}
 
 echo fuseki starting...
 cd $FUSEKI_DIR
