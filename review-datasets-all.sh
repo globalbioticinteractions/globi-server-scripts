ba20db89076c03554d11cb3e6017423bcdc0c0ee
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2022-12-07 07:56:07 -0600

    review 3 datasets in parallel

--- review-datasets-all.sh
+++ review-datasets-all.sh
@@ -6,5 +6,5 @@ set -x
 DATASET_DIR=${ELTON_DATASET_DIR:-/var/cache/elton/datasets}
 DATASETS_LOCAL=$(elton ls --cache-dir "${DATASET_DIR}" --no-progress | sort | uniq | grep -v "^local$")
 
-echo "$DATASETS_LOCAL" | xargs -L1 -I '{}' /bin/bash /var/lib/globinizer/check-dataset.sh '{}' ${DATASET_DIR}
-
+echo "$DATASETS_LOCAL"\
+ | parallel -j3 /var/lib/globinizer/check-dataset.sh {1} ${DATASET_DIR}
