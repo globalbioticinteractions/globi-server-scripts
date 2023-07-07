61f49900ecf57a0cb8ee2ffb714c6730ba9f325a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-07-07 14:44:13 -0500

    allow for taxonomic cache warm up

--- review-datasets-all.sh
+++ review-datasets-all.sh
@@ -6,5 +6,12 @@ set -x
 DATASET_DIR=${ELTON_DATASET_DIR:-/var/cache/elton/datasets}
 DATASETS_LOCAL=$(elton ls --cache-dir "${DATASET_DIR}" --no-progress | sort | uniq | grep -v "^local$")
 
+# first run 10 review in sequence to warm up taxonomic resource caches
 echo "$DATASETS_LOCAL"\
+ | head\
+ | parallel -j1 /var/lib/globinizer/check-dataset.sh {1} ${DATASET_DIR}
+
+# run the rest in parallel against "warm" caches
+echo "$DATASETS_LOCAL"\
+ | tail -n+10\
  | parallel -j3 /var/lib/globinizer/check-dataset.sh {1} ${DATASET_DIR}
