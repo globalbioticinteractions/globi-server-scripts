fd090102843fdd454e3d5d0c2675b2f685135cfe
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-10-02 11:10:17 -0500

    attempt to fix update-index.sh

--- update-index.sh
+++ update-index.sh
@@ -23,7 +23,7 @@ function update {
   GRAPH_DB_VERSION="1.1-SNAPSHOT"
   GRAPH_DB_SNAPSHOT="$MAVEN_REPO/org/eol/eol-globi-datasets/${GRAPH_DB_VERSION}/eol-globi-datasets-${GRAPH_DB_VERSION}-neo4j-graph-db.$GRAPH_DB_EXT"
 
-  if "${FORCE_RESET}" != "-f" && diff $GRAPH_DB_ARCHIVE $GRAPH_DB_SNAPSHOT >/dev/null; then
+  if [ "${FORCE_RESET}" != "-f" && diff $GRAPH_DB_ARCHIVE $GRAPH_DB_SNAPSHOT >/dev/null ]; then
     echo File same, no update needed
   else
     echo File different updating
